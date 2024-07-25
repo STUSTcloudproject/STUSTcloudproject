d2c154d66328ba6e423907d043e345d850734b7b
Jne <giamchiu@gmail.com>
2024-07-25 20:18:33 +0800

    優化 支援沒有虛擬環境的開啟

--- run.bat
+++ run.bat
@@ -1,9 +1,15 @@
 @echo off
-REM 更改工作目录到 src
+REM 更改工作目錄到 src
 cd /d "%~dp0src"
 
-REM 使用虚拟环境中的 Python 解释器运行主程序
-..\env\Scripts\python.exe main.py
+REM 判斷虛擬環境是否存在
+IF EXIST ..\env\Scripts\python.exe (
+    REM 使用虛擬環境中的 Python 直譯器運行主程序
+    ..\env\Scripts\python.exe main.py
+) ELSE (
+    REM 使用系統中的 Python 直譯器運行主程序
+    python main.py
+)
 
-REM 暂停，等待用户按任意键继续
+REM 暫停，等待用戶按任意鍵繼續
 pause
